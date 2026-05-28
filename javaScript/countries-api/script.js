dd3e7ec9929c53763fb7c25830c6f759f800f4b9
almayomekonen <alamayo274@gmail.com>
2026-05-28 18:05:09 +0300

    countries api finished

---
+++ javaScript/countries-api/script.js
@@ -0,0 +1,39 @@
+const countriesContainer = document.getElementById('counriesContainer');
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+
+let allCountries = [];
+
+async function getAllCountries() {
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,currencies,flags,population,symbol,languages,region');
+
+    const countriesData = await response.json();
+
+    console.log(countriesData);
+
+    displayCountries(countriesData);
+}
+
+function displayCountries(countries) {
+   countries.forEach((country) => {
+    countriesContainer.innerHTML += `
+        <div class="country-card">
+           <img src="${country.flags.png}" />
+
+           <div class="country-info"> 
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+           </div>
+        </div>
+        
+        `;
+   })
+}
+
+
+
+
+
+getAllCountries();
